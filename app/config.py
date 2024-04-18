4d7d6a70d0fca7daefbe886114d85c2ba6c5798c
RomanBorovskiy <borovskiyroman@gmail.com>
2024-04-18 20:47:27 +0300

    Добавлены переменные конфигурации

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from pydantic import SecretStr, Field
+from pydantic import SecretStr, Field, PostgresDsn, RedisDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 BASE_DIR = Path(__file__).parent
@@ -11,13 +11,14 @@ PICS_DIR = BASE_DIR / "pics"
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(env_file=BASE_DIR.parent / ".env.local", env_file_encoding="utf-8", extra="allow")
 
-    BOT_TOKEN: SecretStr
-    PAGE_TIMEOUT: int = Field(default=10 * 1000, env="BOT_PAGE_TIMEOUT")  # milliseconds for page load
-    DROP_UPDATES_ON_START: bool Field(default=False, env="BOT_DROP_UPDATES_ON_START")
-    DB_URI = Field(..., env="DB_BOT_DSN")
-    REDIS_URI = Field(..., env="REDIS_BOT_DSN")
+    BOT_TOKEN: SecretStr = Field(..., alias="BOT_TOKEN")
+    PAGE_TIMEOUT: int = Field(default=10 * 1000, alias="BOT_PAGE_TIMEOUT")  # milliseconds for page load
+    DROP_UPDATES_ON_START: bool = Field(default=False, alias="BOT_DROP_UPDATES_ON_START")
+    DB_URI: PostgresDsn = Field(..., alias="DB_BOT_DSN")
+    REDIS_URI: RedisDsn = Field(..., alias="REDIS_BOT_DSN")
+    DEBUG: bool = Field(default=False, alias="BOT_DEBUG")
 
 
 settings = Settings()

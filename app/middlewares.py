548d772b2b16262462552fea978f7c26bb3ab1c4
RomanBorovskiy <borovskiyroman@gmail.com>
2024-04-18 20:45:33 +0300

    Добавил middleware для определения языка

---
+++ app/middlewares.py
@@ -0,0 +1,14 @@
+from aiogram import BaseMiddleware
+from aiogram.types import TelegramObject
+
+from core import get_user_language
+
+
+class LanguageMiddleware(BaseMiddleware):
+    """Middleware для определения языка пользователя"""
+    async def __call__(self, handler: callable, event: TelegramObject, data: dict):
+        user = data["event_from_user"]
+        lang = await get_user_language(user)
+        data["user_language"] = lang
+        result = await handler(event, data)
+        return result

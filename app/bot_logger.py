e4eea8cf7f42614edbc838303eb1c2e2b52649fb
RomanBorovskiy <borovskiyroman@gmail.com>
2024-04-21 19:45:53 +0300

    Добавлен loguru

--- app/bot_logger.py
+++ app/bot_logger.py
@@ -1,9 +1,34 @@
 import logging
+
+from loguru import logger
+
 from config import settings
 
+
+class InterceptHandler(logging.Handler):
+    def emit(self, record: logging.LogRecord) -> None:
+        # Get corresponding Loguru level if it exists.
+        level: str | int
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        # Find caller from where originated the logged message.
+        frame, depth = logging.currentframe(), 0
+        while frame and (depth == 0 or frame.f_code.co_filename == logging.__file__):
+            frame = frame.f_back
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
+
+
+def setup_logger(level: str | int = "DEBUG"):
+    logging.basicConfig(
+        handlers=[InterceptHandler()],
+        level=logging.getLevelName(level)
+    )
+
+
 log_level = logging.DEBUG if settings.DEBUG else logging.INFO
-logging.basicConfig(level=log_level, format="%(asctime)s - %(levelname)s - %(name)s - %(message)s")
-
-
-def get_logger(name):
-    return logging.getLogger(name)
+setup_logger(log_level)
